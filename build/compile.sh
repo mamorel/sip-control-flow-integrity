01e3930bccfa33e43a92f179c27d3614c340b955
kuechleralexander1@gmail.com <kuechleralexander1@gmail.com>
2017-05-23 09:21:41 +0200

    Build shadow stack in StackAnalysis.c and compile something.c with compile script

--- build/compile.sh
+++ build/compile.sh
@@ -1,3 +1,4 @@
+clang-3.9 -emit-llvm -c ../something.c -o ../something.bc
 opt-3.9 -load code/libFunctionPass.so -functionpass < ../something.bc > something_pass.bc
 opt-3.9 -O3 < something_pass.bc > something_opt.bc
 llc-3.9 something_opt.bc
--- code/StackAnalysis.c
+++ code/StackAnalysis.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	char *value;
+	struct node *next;
+} node_t;
+
+node_t *list = NULL;
+
 
 void registerFunction(char functionName[]) {
 	printf("In function: %s\n", functionName);
+
+	node_t *new_node = malloc(sizeof(node_t));
+	new_node->value = functionName;
+	new_node->next = list;
+	list = new_node;
+	printf("Stack: ");
+	node_t *next = list;
+	while(next != NULL) {
+		printf("%s ; ", next->value);
+		next = next->next;
+	}
+	printf("\n");
 }
 
 void deregisterFunction(char functionName[]) {
 	printf("Exit function: %s\n", functionName);
+	
+	if (list == NULL) {
+		fprintf(stderr, "Error: No function on shadow stack that can be poped!\n");
+        return;
+    }
+	if(list->value != functionName) {
+		fprintf(stderr, "Error: Wrong function on top of the shadow stack! Want to pop %s but found %s\n",
+			functionName, list->value);
+		return;
+	}
+
+    node_t *next_node = list->next;
+    free(list);
+    list = next_node;
+	node_t *next = list;
+	printf("Stack: ");
+	while(next != NULL) {
+		printf("%s ; ", next->value);
+		next = next->next;
+	}
+	printf("\n");
 }
 
 
